6aeee15ced9d053e7ba5ca44c4815ad3b189a070
hasantouma <hasantouma@gmail.com>
2021-08-21 19:29:17 -0400

    simplified pretty print and added example file

--- src/pp.ml
+++ src/pp.ml
@@ -7,14 +7,11 @@ let pp_bop bop =
   | BTimes -> "*"
   | BDiv -> "/"
 
-let pp expr =
-  let rec pp_aux expr indent =
-    match expr with
-    | EInt n -> string_of_int n
-    | EBinOp(l, bop, r) ->
-        let left = pp_aux l (indent + 3) in
-        let spaces = String.make (indent + 3) ' ' in
-        let right = pp_aux r (indent + 3) in
-        "(" ^ left ^ "\n" ^ spaces ^ (pp_bop bop) ^ right ^ ")"
-  in pp_aux expr 0
+let rec pp expr =
+  match expr with
+  | EInt n -> string_of_int n
+  | EBinOp(l, bop, r) ->
+      let left = pp l in
+      let right = pp r in
+      "(" ^ left ^ " " ^ (pp_bop bop) ^ " " ^ right ^ ")"
 
