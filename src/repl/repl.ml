598e973180387b74c96ddb342b3e486296696ecd
hasantouma <hasantouma@gmail.com>
2021-08-21 20:07:42 -0400

    simplified printing output. Display pp with output

--- src/repl/repl.ml
+++ src/repl/repl.ml
@@ -15,6 +15,10 @@ let handle_exit repl_in =
   if repl_in = "#quit" then
     exit 0
 
+let handle_display (e : expr) : unit =
+  let answer = string_of_int (interp e) in
+  print_endline ((pp e) ^ " = " ^ answer)
+
 let rec repl () : unit =
   output_string stdout "> ";
   flush stdout;
@@ -22,12 +26,10 @@ let rec repl () : unit =
   handle_exit repl_in;
   let lexbuf = Lexing.from_string repl_in in
   let e : expr = main token lexbuf in
-  print_endline (string_of_int (interp e));
+  handle_display e;
   repl ()
 
 let interp_file (file_name : string) : unit =
   let e : expr = parse_file file_name in
-  print_endline (pp e);
-  let i = interp e in
-  print_endline (string_of_int i)
+  handle_display e
 
